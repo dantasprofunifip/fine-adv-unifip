64004106fa074438cac507fc1e3b66494f55bc14
juniorsilvaads <cicerojunior@ads.fiponline.edu.br>
2022-04-12 21:21:01 -0300

    task01 - cenario 1 - configuracoes

--- app/src/main/java/com/example/myapplication/data/LoginRepository.kt
+++ app/src/main/java/com/example/myapplication/data/LoginRepository.kt
@@ -4,7 +4,7 @@ import com.example.myapplication.data.model.LoggedInUser
 
 /**
  * Class that requests authentication and user information from the remote data source and
- * maintains an in-memory cache of login status and user credentials information.
+ * maintains an in-memory cache of login status and user credentials information. mod
  */
 
 class LoginRepository(val dataSource: LoginDataSource) {
